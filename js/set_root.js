c8ef68ee580b2087f350ac28131e09e42ea19fcb
ynlc <787512217@qq.com>
2018-05-29 23:56:36 +0800

    banner

---
+++ js/set_root.js
@@ -0,0 +1,10 @@
+/*自动计算rem*/
+(function(){
+    var calc = function(){
+        var docElement = document.documentElement;
+        var clientWidthValue = docElement.clientWidth > 750 ? 750 : docElement.clientWidth;
+        docElement.style.fontSize = 20*(clientWidthValue/375) + 'px';
+    }
+    calc();
+    window.addEventListener('resize',calc);
+})();
